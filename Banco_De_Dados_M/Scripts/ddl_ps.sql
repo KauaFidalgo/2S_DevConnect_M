d207e1d31dfc34e71173d7245be81faeb17dca7b
kaua fidalgo <kauafidalgo01@gmail.com>
2025-10-08 08:34:22 -0300

    Finalzado DB Processo Seletivo

--- Banco_De_Dados_M/Scripts/ddl_ps.sql
+++ Banco_De_Dados_M/Scripts/ddl_ps.sql
@@ -1,74 +1,63 @@
---DDL - ESTRUTURA BANCO
+--DDL--
+
 CREATE DATABASE db_devconnect;
+GO 
 
-
-GO
---COMANDO QUE USA O DB
-USE db_devconnect;
-
---TABELA USUARIO
 CREATE TABLE tb_usuario(
-	 id		INT		 IDENTITY(1,1)	 PRIMARY KEY
-	,nome_completo   NVARCHAR(255)           NOT NULL
-	,nome_usuario    NVARCHAR(50)    UNIQUE	 NOT NULL
-	,email           NVARCHAR(255)   UNIQUE  NOT NULL
-	,senha           NVARCHAR(30)            NOT NULL
-	,foto_perfil_url NVARCHAR(150)               NULL
+	id	 INT		IDENTITY(1,1) PRIMARY KEY 
+	,nome_completo   NVARCHAR(255)     NOT NULL
+	,nome_usuario	 NVARCHAR(50)      NOT NULL  UNIQUE
+	,email			 NVARCHAR(255)     NOT NULL  UNIQUE
+	,senha			 NVARCHAR(50)	   NOT NULL
+	,foto_perfil_url NVARCHAR(200)     NULL
+
 );
-GO
 
---CONSULTAR TABELAS
-SELECT * FROM tb_usuario;
-
-
---TABELA PUBLICACAO
 CREATE TABLE tb_publicacao(
-	id		INT		 IDENTITY(1,1)	                PRIMARY KEY
-   ,descricao        NVARCHAR(500)           
-   ,imagem_url       NVARCHAR(150)                  NOT NULL
-   ,data_publicacao  NVARCHAR(8)                    NOT NULL
+  id	             INT		    IDENTITY(1,1) PRIMARY KEY 
+ ,descricao          NVARCHAR(255)                NOT NULL
+ ,imagem_url         NVARCHAR(255)                NULL
+ ,data_publicacao    DATE                         NOT NULL 
+ ,id_usuario         INT                          NOT NULL
 
-   ,id_usuario  INT NOT NULL,
-   
-   FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id)
+ FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id)
 );
 
 SELECT * FROM tb_publicacao;
 
---TABELA CURTIDA
 CREATE TABLE tb_curtida(
-	id		INT		 IDENTITY(1,1)	         PRIMARY KEY
-   ,id_usuario       INT                     NOT NULL,
+id_curtida INT IDENTITY(1,1) PRIMARY KEY,
+id_usuario INT NOT NULL,
+id_publicacao INT NOT NULL,
 
-   FOREIGN KEY (id_usuario)     REFERENCES tb_usuario(id)
+FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id),
 
-   ,id_publicacao    INT                     NOT NULL,
-
-   FOREIGN KEY (id_publicacao)  REFERENCES tb_publicacao(id)
-
+FOREIGN KEY (id_publicacao) REFERENCES tb_publicacao(id),
 );
 
 SELECT * FROM tb_curtida;
 
---TABELA COMENTARIO
 CREATE TABLE tb_comentario(
-	id		INT		 IDENTITY(1,1)	         PRIMARY KEY
-   ,texto            NVARCHAR(255)           NOT NULL
-   ,data_comentario  NVARCHAR(8)             NOT NULL
-   ,id_usuario       INT                     NOT NULL,
+	id_comentario INT IDENTITY(1,1) PRIMARY KEY,
+	texto NVARCHAR(155) NOT NULL,
+	data_coment DATE	NOT NULL,
+	id_usuario INT		NOT NULL,
+	id_publicacao INT	NOT NULL,
 
-   FOREIGN KEY (id_usuario)     REFERENCES tb_usuario(id)
-
-   ,id_publicacao    INT                     NOT NULL,
-
-   FOREIGN KEY (id_publicacao)  REFERENCES tb_publicacao(id)
+	FOREIGN KEY(id_usuario) REFERENCES tb_usuario(id),
+	FOREIGN KEY(id_publicacao) REFERENCES tb_publicacao(id),
 );
 
 SELECT * FROM tb_comentario;
 
-CREATE TABLE tb_seguidor(
-    id_usuario_seguir     INT NOT NULL, --PK
-	id_usuario_seguido    INT NOT NULL, --PK
+	CREATE TABLE tb_seguidor (
+	id_usuario_seguidor INT    NOT NULL,
+	id_seguindo INT     NOT NULL,
 
-	PRIMARY KEY (id_usuario_seguir, id_usuario_seguido)
-);
+	PRIMARY KEY (id_usuario_seguidor, id_seguindo),
+	FOREIGN KEY  (id_usuario_seguidor) REFERENCES  tb_usuario(id),
+    FOREIGN KEY  (id_seguindo) REFERENCES tb_usuario(id)
+	);
+GO
+
+SELECT * FROM tb_seguidor;
--- Banco_De_Dados_M/Scripts/dml_ps.sql
+++ Banco_De_Dados_M/Scripts/dml_ps.sql
@@ -1,35 +1,36 @@
---DML - REGISTRO
+--DML
 USE db_devconnect;
 
---Comando para inserir um novo registro, registro usuario
-INSERT INTO tb_usuario(nome_completo, nome_usuario, email, senha, foto_perfil_url)
-VALUES('Kaua Fidalgo', 'oKakaxz', 'kauafidalgo@gmail.com', '080309', 1)
+INSERT INTO tb_usuario(nome_completo,nome_usuario,email,senha,foto_perfil_url)
+VALUES('Eduardo Costa','educosta','educosta@gmail.com','semsenha','www.com.DevConnect/edu');
 
 SELECT * FROM tb_usuario;
 
---Registro publicacao
-INSERT INTO tb_publicacao(descricao, imagem_url, data_publicacao)
-VALUES('blablabla blebleble', 'www.kauafoto.com.br', '2009/08/03')
+INSERT INTO  tb_publicacao(descricao,imagem_url,data_publicacao, id_usuario)
+VALUES('Dia lindo com a familia','www.fotobrasil.com','2025/10/03',1);
 
 SELECT * FROM tb_publicacao;
 
---Registro Curtida
 INSERT INTO tb_curtida(id_usuario, id_publicacao)
-VALUES(1, 2)
+VALUES (1, 1);
 
 SELECT * FROM tb_curtida;
 
---Registro Comentario
-INSERT INTO tb_comentario(texto, data_comentario, id_usuario, id_publicacao)
-VALUES('alguem', '2021/02/12', 1, 2)
+INSERT INTO tb_comentario(texto, data_coment, id_usuario, id_publicacao)
+VALUES('Que familia linda', '2025-10-10',1, 1);
 
-SELECT * FROM tb_comentario;
+SELECT * FROM tb_comentario
 
---Registro Seguidor - INTERMEDIARIA
-
-INSERT INTO tb_seguidor(id_usuario_seguir, id_usuario_seguido)
-VALUES 
-(1, 2),
-(1, 2);
+INSERT INTO tb_seguidor(id_seguindo, id_usuario_seguidor)
+VALUES(1,2);
 
 SELECT * FROM tb_seguidor;
+
+
+
+SELECT 
+	descricao as PUBLICACAO, 
+	nome_completo AS AUTOR 
+
+FROM tb_publicacao 
+	INNER JOIN tb_usuario ON tb_publicacao.id_usuario = tb_usuario.id;
